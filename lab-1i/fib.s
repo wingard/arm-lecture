e42eb4fd04527fa1c20265b7127d200b8ac40655
Kevin Jiun-An Lin <wingard99@gmail.com>
2015-08-10 04:56:48 +0000

    Implement fibonacci assembly (non-recursive)

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -13,31 +13,21 @@ fibonacci:
 	@ PROLOG
 	push {r3, r4, r5, lr}
 
-	@ R4 = R0 - 0 (update flags)
-	@ if(R0 <= 0) goto .L3 (which returns 0)
+	mov r4, #0
+	mov r5, #1
 
-	@ Compare R4 wtih 1
-	@ If R4 == 1 goto .L4 (which returns 1)
+  .L1:
+	cmp r0, #1
+	bgt .L2
 
-	@ R0 = R4 - 1
-	@ Recursive call to fibonacci with R4 - 1 as parameter
-
-	@ R5 = R0
-	@ R0 = R4 - 2
-	@ Recursive call to fibonacci with R4 - 2 as parameter
-
-	@ R0 = R5 + R0 (update flags)
-
+        mov r0, r5        
 	pop {r3, r4, r5, pc}		@EPILOG
 
-	@ END CODE MODIFICATION
-.L3:
-	mov r0, #0			@ R0 = 0
-	pop {r3, r4, r5, pc}		@ EPILOG
-
-.L4:
-	mov r0, #1			@ R0 = 1
-	pop {r3, r4, r5, pc}		@ EPILOG
-
+  .L2:
+	mov r6, r4
+	mov r4, r5
+	add r5, r5, r6
+	sub r0, r0, #1
+	bl .L1
 	.size fibonacci, .-fibonacci
 	.end
